199e8fa9d37859f40dab81f4467dfa8470b9f6aa
NoBody <sadeeqmauwal@gmail.com>
2025-04-23 20:57:49 +0530

    GEN AI YouTube & Web Summarization

--- 11. Text Summarization With Langchain/app.py
+++ 11. Text Summarization With Langchain/app.py
@@ -9,3 +9,10 @@ from langchain_community.document_loaders import YoutubeLoader, UnstructuredURLL
 st.set_page_config(page_title="LangChain: Summarize Text From YT or website", page_icon="🐦")
 st.title("🐦 LangChain: Summarize Text From YT or Website")
 st.subheader("Summarize URL")
+
+
+## Get the Groq API Key asn URL (YT or website) to be Summarized
+with st.sidebar:
+    groq_api_key = st.text_input("Groq API Key", value="", type="password")
+    
+url = st.text_input("")
