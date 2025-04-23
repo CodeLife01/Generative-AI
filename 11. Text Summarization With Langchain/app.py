a9960f3a66f199e05dd9e5629f6a34c81ca04a75
NoBody <sadeeqmauwal@gmail.com>
2025-04-23 20:55:28 +0530

    GEN AI YouTube & Web Summarization

--- 11. Text Summarization With Langchain/app.py
+++ 11. Text Summarization With Langchain/app.py
@@ -1,3 +1,11 @@
 import validators, streamlit as st
 from langchain.prompts import PromptTemplate
 from langchain_groq import ChatGroq
+from langchain.chains.summarize import load_summarize_chain
+from langchain_community.document_loaders import YoutubeLoader, UnstructuredURLLoader
+
+
+## Streamlit APP
+st.set_page_config(page_title="LangChain: Summarize Text From YT or website", page_icon="🐦")
+st.title("🐦 LangChain: Summarize Text From YT or Website")
+st.subheader("Summarize URL")
