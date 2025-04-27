c2abe354dc4410588827a25b6fc5374262e06370
NoBody <sadeeqmauwal@gmail.com>
2025-04-27 14:39:13 +0530

    GEN AI YouTube & Web Summarization

--- 11. Text Summarization With Langchain/app.py
+++ 11. Text Summarization With Langchain/app.py
@@ -18,7 +18,7 @@ with st.sidebar:
 url = st.text_input("URL", label_visibility="collapsed")
 
 ## Gemma Model Using Groq API
-llm = ChatGroq(model="Gemma-7b-It", groq_api_key=groq_api_key)
+llm = ChatGroq(model="llama-3.3-70b-versatile", groq_api_key=groq_api_key)
 
 prompt_template = """
 Provide summarize of the following content in 300 words:
