ad19764d156ec01fd449086ae6ac31bb6993a379
NoBody <sadeeqmauwal@gmail.com>
2025-04-27 21:43:00 +0530

    GEN AI YouTube & Web Summarization

--- 11. Text Summarization With Langchain/app.py
+++ 11. Text Summarization With Langchain/app.py
@@ -1,56 +1,56 @@
-import validators, streamlit as st
+import validators,streamlit as st
 from langchain.prompts import PromptTemplate
 from langchain_groq import ChatGroq
 from langchain.chains.summarize import load_summarize_chain
-from langchain_community.document_loaders import YoutubeLoader, UnstructuredURLLoader
+from langchain_community.document_loaders import YoutubeLoader,UnstructuredURLLoader
 
 
-## Streamlit APP
-st.set_page_config(page_title="LangChain: Summarize Text From YT or website", page_icon="🐦")
-st.title("🐦 LangChain: Summarize Text From YT or Website")
-st.subheader("Summarize URL")
+## sstreamlit APP
+st.set_page_config(page_title="LangChain: Summarize Text From YT or Website", page_icon="🦜")
+st.title("🦜 LangChain: Summarize Text From YT or Website")
+st.subheader('Summarize URL')
 
 
-## Get the Groq API Key asn URL (YT or website) to be Summarized
+
+## Get the Groq API Key and url(YT or website)to be summarized
 with st.sidebar:
-    groq_api_key = st.text_input("Groq API Key", value="", type="password")
+    groq_api_key=st.text_input("Groq API Key",value="",type="password")
 
-url = st.text_input("URL", label_visibility="collapsed")
+generic_url=st.text_input("URL",label_visibility="collapsed")
 
-## Gemma Model Using Groq API
-llm = ChatGroq(model="llama-3.3-70b-versatile", groq_api_key=groq_api_key)
+## Gemma Model USsing Groq API
+llm =ChatGroq(model="Gemma-7b-It", groq_api_key=groq_api_key)
 
-prompt_template = """
-Provide summarize of the following content in 300 words:
+prompt_template="""
+Provide a summary of the following content in 300 words:
 Content:{text}
 
 """
+prompt=PromptTemplate(template=prompt_template,input_variables=["text"])
 
-prompt = PromptTemplate(template=prompt_template, input_variables=["text"])
-
-if st.button("Summarizee the Content from YT or Website"):
+if st.button("Summarize the Content from YT or Website"):
     ## Validate all the inputs
-    if not groq_api_key.strip() or not url.strip():
+    if not groq_api_key.strip() or not generic_url.strip():
         st.error("Please provide the information to get started")
-    elif not validators.url(url):
-        st.error("Please enter a valid URL. It can may be YT video url or website url")
-        
+    elif not validators.url(generic_url):
+        st.error("Please enter a valid Url. It can may be a YT video utl or website url")
+
     else:
         try:
             with st.spinner("Waiting..."):
-                ## loading the website or YT video data
-                if "youtube.com" in url:
-                    loader = YoutubeLoader.from_youtube_url(url, add_video_info=True)
+                ## loading the website or yt video data
+                if "youtube.com" in generic_url:
+                    loader=YoutubeLoader.from_youtube_url(generic_url,add_video_info=True)
                 else:
-                    loader = UnstructuredURLLoader(urls=[url], ssl_verify=False,
-                                                   header = {"User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 13_5_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/116.0.0.0 Safari/537.36"})
-                docs = loader.load()
-                
-                ## Chain for summarization
-                chain = load_summarize_chain(llm, chain_type="stuff", prompt=prompt)
-                output_summary = chain.run(docs)
-                
+                    loader=UnstructuredURLLoader(urls=[generic_url],ssl_verify=False,
+                                                 headers={"User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 13_5_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/116.0.0.0 Safari/537.36"})
+                docs=loader.load()
+
+                ## Chain For Summarization
+                chain=load_summarize_chain(llm,chain_type="stuff",prompt=prompt)
+                output_summary=chain.run(docs)
+
                 st.success(output_summary)
-                
         except Exception as e:
             st.exception(f"Exception:{e}")
+                    
