2123a88a8701394792ce8967e6a14ffb92698a1b
NoBody <sadeeqmauwal@gmail.com>
2025-05-02 15:54:15 +0530

    Updating app.py

--- 16. Gen AI with AWS/app.py
+++ 16. Gen AI with AWS/app.py
@@ -2,6 +2,7 @@ import json
 import os
 import sys
 import boto3
+import streamlit as st
 
 ## We will be Using Titan Embeddings Model to generate Embedding
 
@@ -79,4 +80,42 @@ PROMPT = PromptTemplate(
 )
 
 def get_response_llm(llm, vectorstore_faiss, query):
+    qa = RetrievalQA.from_chain_type(
+        llm=llm,
+        chain_type="stuff",
+        retriever=vectorstore_faiss.as_retriever(
+            search_type="similarity", search_kwargs={"k":3}
+        ),
+        return_source_documents=True,
+        chain_type_kwargs={"prompt":PROMPT}
+    )
+
+    answer=qa({"query":query})
+    return answer["result"]
     
+
+def main():
+    st.set_page_config("Chat PDF")
+    st.header("Chat with PDF using AWS Bedrock")
+    
+    user_question = st.text_input("Ask a Question from the PDF Files")
+    with st.sidebar:
+        st.title("Update Or Create Vector Store")
+        
+        if st.button("Vector Update"):
+            with st.spinner("Processing..."):
+                docs = data_ingestion()
+                get_vector_store(docs)
+                st.success("Done")
+                
+    if st.button("Claude Output"):
+        with st.spinner("Processing..."):
+            faiss_index = FAISS.load_local("faiss_index", bedrock_embeddings)
+            llm = get_claude_llm()
+            
+            ## Faiss_index = get_vector_store(docs)
+            st.write(get_response_llm(llm, faiss_index, user_question))
+
+            
+if __name__== "__main__":
+    main()
