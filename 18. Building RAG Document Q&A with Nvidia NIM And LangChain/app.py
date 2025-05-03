9f8f57514ce548647d4a0b8a8baf0437b925198d
NoBody <sadeeqmauwal@gmail.com>
2025-05-03 13:49:48 +0530

    Building RAG with Nvidia & Langchain

---
+++ 18. Building RAG Document Q&A with Nvidia NIM And LangChain/app.py
@@ -0,0 +1,23 @@
+from openai import OpenAI
+import os
+from dotenv import load_dotenv
+load_dotenv()
+
+client = OpenAI(
+  base_url = "https://integrate.api.nvidia.com/v1",
+  api_key = "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"
+)
+
+completion = client.chat.completions.create(
+  model="meta/llama-3.1-70b-instruct",
+  messages=[{"role":"user","content":"provide me an essay article on machine learning"}],
+  temperature=0.2,
+  top_p=0.7,
+  max_tokens=1024,
+  stream=True
+)
+
+for chunk in completion:
+  if chunk.choices[0].delta.content is not None:
+    print(chunk.choices[0].delta.content, end="")
+
